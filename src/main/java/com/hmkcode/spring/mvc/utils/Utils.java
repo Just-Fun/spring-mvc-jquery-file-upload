74871a4c9f367c9ed85e828b7fbce5ddd05fa4b9
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-19 22:22:29 +0300

    move Properties separate to class

---
+++ src/main/java/com/hmkcode/spring/mvc/utils/Utils.java
@@ -0,0 +1,61 @@
+package com.hmkcode.spring.mvc.utils;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * Created by Serzh on 10/19/16.
+ */
+public class Utils {
+
+    private final String PROPERTIES_FILE = "src/main/resources/config.properties";
+    private String host;
+    private String port;
+
+    private String database;
+    private String userName;
+    private String password;
+
+    public Utils() {
+        loadProperties();
+
+    }
+
+    private void loadProperties() {
+        Properties property = new Properties();
+        try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)) {
+            property.load(fis);
+            host = property.getProperty("host");
+            port = property.getProperty("port");
+            database = property.getProperty("database");
+            userName = property.getProperty("userName");
+            password = property.getProperty("password");
+        } catch (IOException e) {
+            throw new RuntimeException("Some trouble with loading properties: " + e.getCause());
+        }
+    }
+
+    public  String getHost() {
+        return host;
+    }
+
+    public  String getPort() {
+        return port;
+    }
+
+    public  String getDatabase() {
+        return database;
+    }
+
+    public  String getUserName() {
+        return userName;
+    }
+
+    public  String getPassword() {
+        return password;
+    }
+}
