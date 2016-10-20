aca8a7d4b6843ef96c043e9f028556b96f1bbd51
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-20 13:20:39 +0300

    add ServiceTest

--- src/test/java/com.hmkcode.spring.mvc/inegration/ServiceTest.java
+++ src/test/java/com.hmkcode.spring.mvc/inegration/ServiceTest.java
@@ -1,8 +1,13 @@
 package com.hmkcode.spring.mvc.inegration;
 
+import com.hmkcode.spring.mvc.service.Service;
 import com.hmkcode.spring.mvc.utils.Setup;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.Assert.*;
 
@@ -10,17 +15,36 @@ import static org.junit.Assert.*;
  * Created by Serzh on 10/18/16.
  */
 public class ServiceTest {
+    private static Setup setup;
+    private Service service;
 
     @BeforeClass
     public static void setup() {
-        Setup setup = new Setup();
+        setup = new Setup();
         setup.createData();
-       /* manager = new PostgreSQLManager();
-        setup.setupData(manager, DATABASE);*/
     }
-    @Test
-    public void run() throws Exception {
 
+    @AfterClass
+    public static void dropDatabase() {
+        setup.dropData();
+    }
+
+    @Test
+    public void testServiceOneFile() throws Exception {
+        service = new Service();
+        Map<String, Integer> result = service.run(setup.getSesson1());
+
+        assertEquals("{First File!!!=1, First row=1, Second row=1}", result.toString());
+    }
+
+    @Test
+    public void testServiceTwoFiles() throws Exception {
+        service = new Service();
+        Map<String, Integer> result = service.run(setup.getSesson2());
+        Map<String, Integer> map = new TreeMap<>(result);
+
+        assertEquals("{First row=2, Second File!!!=1, Second row=2, Third File!!!=1, Third row=1}",
+                map.toString());
     }
 
 }
