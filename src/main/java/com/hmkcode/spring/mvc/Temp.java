2a0089342519c8bbfb107dfe4154469cfa733ff6
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-20 13:22:33 +0300

    create sql for tables

--- src/main/java/com/hmkcode/spring/mvc/Temp.java
+++ src/main/java/com/hmkcode/spring/mvc/Temp.java
@@ -3,7 +3,7 @@ package com.hmkcode.spring.mvc;
 /**
  * Created by Serzh on 10/14/16.
  */
-public class Test {
+public class Temp {
 
     public static void main(String[] args) {
     }
--- src/main/resources/sql/v1.sql
+++ src/main/resources/sql/v1.sql
@@ -1,4 +1,12 @@
+CREATE TABLE files(
+  id SERIAL PRIMARY KEY,
+  name CHAR(50) NOT NULL,
+  file BYTEA,
+  session BIGINT
+)
+
 CREATE TABLE results (
-  ID  SERIAL PRIMARY KEY,
-  result bytea
+  id  SERIAL PRIMARY KEY,
+  session BIGINT,
+  result BYTEA
 )
